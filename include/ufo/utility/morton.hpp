b3be9eceaa1f7afe9ba0e34ad4b8a5b438cb51d6
Daniel Duberg <danielduberg@gmail.com>
2024-06-11 23:39:22 +0200

    Made into a class instead of free functions

--- include/ufo/utility/morton.hpp
+++ include/ufo/utility/morton.hpp
@@ -44,6 +44,7 @@
 
 // STL
 #include <array>
+#include <cstddef>
 #include <cstdint>
 
 #if defined(UFO_BMI2)
@@ -56,189 +57,224 @@
 
 namespace ufo
 {
-static constexpr std::uint_fast64_t MORTON2_X_MASK = 0x5555555555555555;
-static constexpr std::uint_fast64_t MORTON2_Y_MASK = 0xAAAAAAAAAAAAAAAA;
+template <std::size_t Dim>
+struct Morton;
 
-static constexpr std::uint_fast64_t MORTON3_X_MASK = 0x1249249249249249;
-static constexpr std::uint_fast64_t MORTON3_Y_MASK = 0x2492492492492492;
-static constexpr std::uint_fast64_t MORTON3_Z_MASK = 0x4924924924924924;
+template <>
+struct Morton<1> {
+	static constexpr std::uint_fast64_t const X_MASK = 0xffffffffffffffff;
 
-static constexpr std::uint_fast64_t MORTON4_X_MASK = 0x1111111111111111;
-static constexpr std::uint_fast64_t MORTON4_Y_MASK = 0x2222222222222222;
-static constexpr std::uint_fast64_t MORTON4_Z_MASK = 0x4444444444444444;
-static constexpr std::uint_fast64_t MORTON4_W_MASK = 0x8888888888888888;
+	[[nodiscard]] static constexpr std::uint_fast64_t encode(std::uint_fast64_t x)
+	{
+		return x;
+	}
 
-[[nodiscard]] constexpr std::uint_fast64_t mortonSpread2(std::uint_fast32_t v)
-{
+	[[nodiscard]] static constexpr std::uint_fast64_t decode(std::uint_fast64_t c)
+	{
+		return c;
+	}
+
+	[[nodiscard]] static constexpr std::uint_fast64_t spread(std::uint_fast64_t v)
+	{
+		return v;
+	}
+
+	[[nodiscard]] static constexpr std::uint_fast64_t compact(std::uint_fast64_t x)
+	{
+		return x;
+	}
+};
+
+template <>
+struct Morton<2> {
+	static constexpr std::uint_fast64_t const X_MASK = 0x5555555555555555;
+	static constexpr std::uint_fast64_t const Y_MASK = 0xAAAAAAAAAAAAAAAA;
+
+	[[nodiscard]] static constexpr std::uint_fast64_t encode(std::uint_fast32_t x,
+	                                                         std::uint_fast32_t y)
+	{
 #if defined(UFO_BMI2)
-	return _pdep_u64(static_cast<std::uint_fast64_t>(x), MORTON2_X_MASK);
+		return _pdep_u64(static_cast<std::uint_fast64_t>(x), X_MASK) |
+		       _pdep_u64(static_cast<std::uint_fast64_t>(y), Y_MASK);
 #else
-	std::uint_fast64_t x(v);
-	x &= 0xFFFFFFFF;
-	x = (x | (x << 16)) & 0x0000FFFF0000FFFF;
-	x = (x | (x << 8)) & 0x00FF00FF00FF00FF;
-	x = (x | (x << 4)) & 0x0F0F0F0F0F0F0F0F;
-	x = (x | (x << 2)) & 0x3333333333333333;
-	x = (x | (x << 1)) & 0x5555555555555555;
-	return x;
+		return spread(x) | (spread(y) << 1);
 #endif
-}
+	}
 
-[[nodiscard]] constexpr std::uint_fast64_t mortonSpread3(std::uint_fast32_t v)
-{
+	[[nodiscard]] static constexpr std::array<std::uint_fast32_t, 2> decode(
+	    std::uint_fast64_t x)
+	{
 #if defined(UFO_BMI2)
-	return _pdep_u64(static_cast<std::uint_fast64_t>(x), MORTON3_X_MASK);
+		return {static_cast<std::uint_fast32_t>(_pext_u64(x, X_MASK)),
+		        static_cast<std::uint_fast32_t>(_pext_u64(x, Y_MASK))};
 #else
-	std::uint_fast64_t x(v);
-	x &= 0x1FFFFF;
-	x = (x | x << 32) & 0x1F00000000FFFF;
-	x = (x | x << 16) & 0x1F0000FF0000FF;
-	x = (x | x << 8) & 0x100F00F00F00F00F;
-	x = (x | x << 4) & 0x10C30C30C30C30C3;
-	x = (x | x << 2) & 0x1249249249249249;
-	return x;
+		return {compact(x), compact(x >> 1)};
 #endif
-}
+	}
 
-[[nodiscard]] constexpr std::uint_fast64_t mortonSpread4(std::uint_fast16_t v)
-{
+	[[nodiscard]] static constexpr std::uint_fast64_t spread(std::uint_fast32_t v)
+	{
 #if defined(UFO_BMI2)
-	return _pdep_u64(static_cast<std::uint_fast64_t>(x), MORTON4_X_MASK);
+		return _pdep_u64(static_cast<std::uint_fast64_t>(x), X_MASK);
 #else
-	std::uint_fast64_t x(v);
-	x &= 0xFFFF;
-	x = (x | (x << 32)) & 0xF800000007FF;
-	x = (x | (x << 16)) & 0xF80007C0003F;
-	x = (x | (x << 8)) & 0xC0380700C03807;
-	x = (x | (x << 4)) & 0x843084308430843;
-	x = (x | (x << 2)) & 0x909090909090909;
-	x = (x | (x << 1)) & 0x1111111111111111;
-	return x;
+		std::uint_fast64_t x(v);
+		x &= 0xFFFFFFFF;
+		x = (x | (x << 16)) & 0x0000FFFF0000FFFF;
+		x = (x | (x << 8)) & 0x00FF00FF00FF00FF;
+		x = (x | (x << 4)) & 0x0F0F0F0F0F0F0F0F;
+		x = (x | (x << 2)) & 0x3333333333333333;
+		x = (x | (x << 1)) & 0x5555555555555555;
+		return x;
 #endif
-}
+	}
 
-[[nodiscard]] constexpr std::uint_fast32_t mortonCompact2(std::uint_fast64_t x)
-{
+	[[nodiscard]] static constexpr std::uint_fast32_t compact(std::uint_fast64_t x)
+	{
 #if defined(UFO_BMI2)
-	return static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON2_X_MASK));
+		return static_cast<std::uint_fast32_t>(_pext_u64(x, X_MASK));
 #else
-	x &= 0x5555555555555555;
-	x = (x ^ (x >> 1)) & 0x3333333333333333;
-	x = (x ^ (x >> 2)) & 0x0F0F0F0F0F0F0F0F;
-	x = (x ^ (x >> 4)) & 0x00FF00FF00FF00FF;
-	x = (x ^ (x >> 8)) & 0x0000FFFF0000FFFF;
-	x = (x ^ (x >> 16)) & 0x00000000FFFFFFFF;
-	return static_cast<std::uint_fast32_t>(x);
+		x &= 0x5555555555555555;
+		x = (x ^ (x >> 1)) & 0x3333333333333333;
+		x = (x ^ (x >> 2)) & 0x0F0F0F0F0F0F0F0F;
+		x = (x ^ (x >> 4)) & 0x00FF00FF00FF00FF;
+		x = (x ^ (x >> 8)) & 0x0000FFFF0000FFFF;
+		x = (x ^ (x >> 16)) & 0x00000000FFFFFFFF;
+		return static_cast<std::uint_fast32_t>(x);
 #endif
-}
+	}
+};
 
-[[nodiscard]] constexpr std::uint_fast32_t mortonCompact3(std::uint_fast64_t x)
-{
-#if defined(UFO_BMI2)
-	return static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON3_X_MASK));
-#else
-	x &= 0x1249249249249249;
-	x = (x ^ (x >> 2)) & 0x10C30C30C30C30C3;
-	x = (x ^ (x >> 4)) & 0x100F00F00F00F00F;
-	x = (x ^ (x >> 8)) & 0x1F0000FF0000FF;
-	x = (x ^ (x >> 16)) & 0x1F00000000FFFF;
-	x = (x ^ (x >> 32)) & 0x1FFFFF;
-	return static_cast<std::uint_fast32_t>(x);
-#endif
-}
+template <>
+struct Morton<3> {
+	static constexpr std::uint_fast64_t const X_MASK = 0x1249249249249249;
+	static constexpr std::uint_fast64_t const Y_MASK = 0x2492492492492492;
+	static constexpr std::uint_fast64_t const Z_MASK = 0x4924924924924924;
 
-[[nodiscard]] constexpr std::uint_fast16_t mortonCompact4(std::uint_fast64_t x)
-{
+	[[nodiscard]] static constexpr std::uint_fast64_t encode(std::uint_fast32_t x,
+	                                                         std::uint_fast32_t y,
+	                                                         std::uint_fast32_t z)
+	{
 #if defined(UFO_BMI2)
-	return static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON4_X_MASK));
+		return _pdep_u64(static_cast<std::uint_fast64_t>(x), X_MASK) |
+		       _pdep_u64(static_cast<std::uint_fast64_t>(y), Y_MASK) |
+		       _pdep_u64(static_cast<std::uint_fast64_t>(z), Z_MASK);
 #else
-	x &= 0x1111111111111111;
-	x = (x ^ (x >> 1)) & 0x909090909090909;
-	x = (x ^ (x >> 2)) & 0x843084308430843;
-	x = (x ^ (x >> 4)) & 0xC0380700C03807;
-	x = (x ^ (x >> 8)) & 0xF80007C0003F;
-	x = (x ^ (x >> 16)) & 0xF800000007FF;
-	x = (x ^ (x >> 32)) & 0xFFFF;
-	return static_cast<std::uint_fast16_t>(x);
+		return spread(x) | (spread(y) << 1) | (spread(z) << 2);
 #endif
-}
+	}
 
-[[nodiscard]] constexpr std::uint_fast64_t mortonEncode2(std::uint_fast32_t x,
-                                                         std::uint_fast32_t y)
-{
+	[[nodiscard]] static constexpr std::array<std::uint_fast32_t, 3> decode(
+	    std::uint_fast64_t x)
+	{
 #if defined(UFO_BMI2)
-	return _pdep_u64(static_cast<std::uint_fast64_t>(x), MORTON2_X_MASK) |
-	       _pdep_u64(static_cast<std::uint_fast64_t>(y), MORTON2_Y_MASK);
+		return {static_cast<std::uint_fast32_t>(_pext_u64(x, X_MASK)),
+		        static_cast<std::uint_fast32_t>(_pext_u64(x, Y_MASK)),
+		        static_cast<std::uint_fast32_t>(_pext_u64(x, Z_MASK))};
 #else
-	return mortonSpread2(x) | (mortonSpread2(y) << 1);
+		return {compact(x), compact(x >> 1), compact(x >> 2)};
 #endif
-}
+	}
 
-[[nodiscard]] constexpr std::uint_fast64_t mortonEncode3(std::uint_fast32_t x,
-                                                         std::uint_fast32_t y,
-                                                         std::uint_fast32_t z)
-{
+	[[nodiscard]] static constexpr std::uint_fast64_t spread(std::uint_fast32_t v)
+	{
 #if defined(UFO_BMI2)
-	return _pdep_u64(static_cast<std::uint_fast64_t>(x), MORTON3_X_MASK) |
-	       _pdep_u64(static_cast<std::uint_fast64_t>(y), MORTON3_Y_MASK) |
-	       _pdep_u64(static_cast<std::uint_fast64_t>(z), MORTON3_Z_MASK);
+		return _pdep_u64(static_cast<std::uint_fast64_t>(x), X_MASK);
 #else
-	return mortonSpread3(x) | (mortonSpread3(y) << 1) | (mortonSpread3(z) << 2);
+		std::uint_fast64_t x(v);
+		x &= 0x1FFFFF;
+		x = (x | x << 32) & 0x1F00000000FFFF;
+		x = (x | x << 16) & 0x1F0000FF0000FF;
+		x = (x | x << 8) & 0x100F00F00F00F00F;
+		x = (x | x << 4) & 0x10C30C30C30C30C3;
+		x = (x | x << 2) & 0x1249249249249249;
+		return x;
 #endif
-}
+	}
 
-[[nodiscard]] constexpr std::uint_fast64_t mortonEncode4(std::uint_fast16_t x,
-                                                         std::uint_fast16_t y,
-                                                         std::uint_fast16_t z,
-                                                         std::uint_fast16_t w)
-{
+	[[nodiscard]] static constexpr std::uint_fast32_t compact(std::uint_fast64_t x)
+	{
 #if defined(UFO_BMI2)
-	return _pdep_u64(static_cast<std::uint_fast64_t>(x), MORTON4_X_MASK) |
-	       _pdep_u64(static_cast<std::uint_fast64_t>(y), MORTON4_Y_MASK) |
-	       _pdep_u64(static_cast<std::uint_fast64_t>(z), MORTON4_Z_MASK) |
-	       _pdep_u64(static_cast<std::uint_fast64_t>(w), MORTON4_W_MASK);
+		return static_cast<std::uint_fast32_t>(_pext_u64(x, X_MASK));
 #else
-	return mortonSpread4(x) | (mortonSpread4(y) << 1) | (mortonSpread4(z) << 2) |
-	       (mortonSpread4(w) << 3);
+		x &= 0x1249249249249249;
+		x = (x ^ (x >> 2)) & 0x10C30C30C30C30C3;
+		x = (x ^ (x >> 4)) & 0x100F00F00F00F00F;
+		x = (x ^ (x >> 8)) & 0x1F0000FF0000FF;
+		x = (x ^ (x >> 16)) & 0x1F00000000FFFF;
+		x = (x ^ (x >> 32)) & 0x1FFFFF;
+		return static_cast<std::uint_fast32_t>(x);
 #endif
-}
+	}
+};
 
-[[nodiscard]] constexpr std::array<std::uint_fast32_t, 2> mortonDecode2(
-    std::uint_fast64_t x)
-{
-#if defined(UFO_BMI2)
-	return {static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON2_X_MASK)),
-	        static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON2_Y_MASK))};
-#else
-	return {mortonCompact2(x), mortonCompact2(x >> 1)};
-#endif
-}
+template <>
+struct Morton<4> {
+	static constexpr std::uint_fast64_t const X_MASK = 0x1111111111111111;
+	static constexpr std::uint_fast64_t const Y_MASK = 0x2222222222222222;
+	static constexpr std::uint_fast64_t const Z_MASK = 0x4444444444444444;
+	static constexpr std::uint_fast64_t const W_MASK = 0x8888888888888888;
 
-[[nodiscard]] constexpr std::array<std::uint_fast32_t, 3> mortonDecode3(
-    std::uint_fast64_t x)
-{
+	[[nodiscard]] static constexpr std::uint_fast64_t encode(std::uint_fast16_t x,
+	                                                         std::uint_fast16_t y,
+	                                                         std::uint_fast16_t z,
+	                                                         std::uint_fast16_t w)
+	{
 #if defined(UFO_BMI2)
-	return {static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON3_X_MASK)),
-	        static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON3_Y_MASK)),
-	        static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON3_Z_MASK))};
+		return _pdep_u64(static_cast<std::uint_fast64_t>(x), X_MASK) |
+		       _pdep_u64(static_cast<std::uint_fast64_t>(y), Y_MASK) |
+		       _pdep_u64(static_cast<std::uint_fast64_t>(z), Z_MASK) |
+		       _pdep_u64(static_cast<std::uint_fast64_t>(w), W_MASK);
 #else
-	return {mortonCompact3(x), mortonCompact3(x >> 1), mortonCompact3(x >> 2)};
+		return spread(x) | (spread(y) << 1) | (spread(z) << 2) | (spread(w) << 3);
 #endif
-}
+	}
 
-[[nodiscard]] constexpr std::array<std::uint_fast16_t, 4> mortonDecode4(
-    std::uint_fast64_t x)
-{
+	[[nodiscard]] static constexpr std::array<std::uint_fast16_t, 4> decode(
+	    std::uint_fast64_t x)
+	{
 #if defined(UFO_BMI2)
-	return {static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON4_X_MASK)),
-	        static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON4_Y_MASK)),
-	        static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON4_Z_MASK)),
-	        static_cast<std::uint_fast32_t>(_pext_u64(x, MORTON4_W_MASK))};
+		return {static_cast<std::uint_fast32_t>(_pext_u64(x, X_MASK)),
+		        static_cast<std::uint_fast32_t>(_pext_u64(x, Y_MASK)),
+		        static_cast<std::uint_fast32_t>(_pext_u64(x, Z_MASK)),
+		        static_cast<std::uint_fast32_t>(_pext_u64(x, W_MASK))};
 #else
-	return {mortonCompact4(x), mortonCompact4(x >> 1), mortonCompact4(x >> 2),
-	        mortonCompact4(x >> 3)};
+		return {compact(x), compact(x >> 1), compact(x >> 2), compact(x >> 3)};
 #endif
-}
+	}
+
+	[[nodiscard]] static constexpr std::uint_fast64_t spread(std::uint_fast16_t v)
+	{
+#if defined(UFO_BMI2)
+		return _pdep_u64(static_cast<std::uint_fast64_t>(x), X_MASK);
+#else
+		std::uint_fast64_t x(v);
+		x &= 0xFFFF;
+		x = (x | (x << 32)) & 0xF800000007FF;
+		x = (x | (x << 16)) & 0xF80007C0003F;
+		x = (x | (x << 8)) & 0xC0380700C03807;
+		x = (x | (x << 4)) & 0x843084308430843;
+		x = (x | (x << 2)) & 0x909090909090909;
+		x = (x | (x << 1)) & 0x1111111111111111;
+		return x;
+#endif
+	}
+
+	[[nodiscard]] static constexpr std::uint_fast16_t compact(std::uint_fast64_t x)
+	{
+#if defined(UFO_BMI2)
+		return static_cast<std::uint_fast32_t>(_pext_u64(x, X_MASK));
+#else
+		x &= 0x1111111111111111;
+		x = (x ^ (x >> 1)) & 0x909090909090909;
+		x = (x ^ (x >> 2)) & 0x843084308430843;
+		x = (x ^ (x >> 4)) & 0xC0380700C03807;
+		x = (x ^ (x >> 8)) & 0xF80007C0003F;
+		x = (x ^ (x >> 16)) & 0xF800000007FF;
+		x = (x ^ (x >> 32)) & 0xFFFF;
+		return static_cast<std::uint_fast16_t>(x);
+#endif
+	}
+};
 }  // namespace ufo
 
 #endif  // UFO_UTILITY_MORTON_HPP
